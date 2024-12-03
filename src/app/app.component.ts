b79023e2e616fa23030a886284c0a85815747396
pasanmalli <subasingheimal@gmail.com>
2024-12-03 22:36:27 +0530

    customer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './common/nav/nav.component';
+import { RegisterComponent } from './page/register/register.component';
+import { ViewComponent } from './page/view/view.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,RegisterComponent,ViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
